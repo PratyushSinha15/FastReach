a91f261eb057de2a8da41b52380fa39fa1eaad07
Pratyush Sinha <122526404+GitPratyush@users.noreply.github.com>
2024-08-06 15:21:47 +0530

    Initialized Frontend

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,20 @@
+
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+
+
+}
+
+export default App
